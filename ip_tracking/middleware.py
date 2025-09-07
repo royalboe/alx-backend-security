7ab78c8381482ba9eae5726ca762730fe748d88a
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-09-07 13:31:52 +0100

    added models and middleware

---
+++ ip_tracking/middleware.py
@@ -0,0 +1,30 @@
+
+
+from .models import RequestLog
+
+class IPTrackingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        ip_address = self.get_client_ip(request)
+        path = request.path
+
+        user = request.user if request.user.is_authenticated else None
+        # Log the request
+        RequestLog.objects.create(
+            user=user,
+            ip_address=ip_address, 
+            path=path
+            )
+
+        response = self.get_response(request)
+        return response
+
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
